b4e3944ad6795502e18df3cfeac5b04ea61fe1f0
Garv767 <garvrahut@gmail.com>
2026-07-07 12:02:23 +0530

    perf: optimize rendering cycle

--- webapp/fix_git_page.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/app/dashboard/page.tsx', 'utf8');
-content = content.replace('                 courseSlotMap={data.courseSlotMap} \r\n', '');
-content = content.replace('                 courseSlotMap={data.courseSlotMap} \n', '');
-content = content.replace('                 courseSlotMap={data.courseSlotMap}\r\n', '');
-content = content.replace('                 courseSlotMap={data.courseSlotMap}\n', '');
-fs.writeFileSync('src/app/dashboard/page.tsx', content, 'utf8');
-console.log("FIXED");
